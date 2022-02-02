0558b8f2190cc077410eccbf65323bfc69260c6d
Yasser30007 <234880@rocfriesepoort.nl>
2022-02-02 11:47:05 +0100

    .

--- versturen.php
+++ versturen.php
@@ -20,12 +20,12 @@ click collect snack";
 
 if(mail(implode(',',$receiver), $subject, $body)){
   ?>
-  <META HTTP-EQUIV="Refresh" CONTENT="0; URL=https://p21t4.lesonline.nu/adminoverzicht.php">
+  <META HTTP-EQUIV="Refresh" CONTENT="0; URL=cart.php">
   <?php
 }
 
 
-include("conn.php");
+include("connection.php");
 error_reporting(0);
 
 $query1 = "DELETE FROM bestelling WHERE naam='$naam' && totaal='$totaal' && telef='$telef' && email='$email'  && datum='$datum'  && adres='$adres'  && tijd='$tijd'  && id='$ID' ";
@@ -35,7 +35,7 @@ $data1=mysqli_query($conn, $query1);
 if($data1){
     echo "<script>alert('Bestelling is verzonden')</script>";
     ?>
-    <META HTTP-EQUIV="Refresh" CONTENT="0; URL=https://p21t4.lesonline.nu/adminoverzicht.php">
+    <META HTTP-EQUIV="Refresh" CONTENT="0; URL=cart.php">
     <?php
 }else{
     echo "<script>alert('Sorry, bestelling versturen is mislukt, Probeer het later opnieuw')</script>";
