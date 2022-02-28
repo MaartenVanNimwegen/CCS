beec864359dfabfe19ef344f048143db7d513bf0
Remon Dollenkamp <remon2004@outlook.com>
2022-02-28 16:10:04 +0100

    s

--- artikelbewerken.php
+++ artikelbewerken.php
@@ -9,30 +9,28 @@
 </head>
 <body>
 <?php
-$id= $_GET['id'];
-$sql= "SELECT * FROM products where id=$id";
-$res = $conn->query($sql);
-if ($res) {
-  foreach ($res as $result) {
-echo"
-
-
-
-
-    <div class='container form-popup' id='bewerk'>
-        <div class='form'>
-            <form action='artikelverwijderen.php' method='post' enctype='multipart/form-data'
-                <input type='text' name='name' class='input' placeholder='Naam'><br>
-                <input class='input' type='name' name='code' placeholder='Code'><br>
-                <input class='input' type='name' name='price' placeholder='Prijs'><br>
-                <input class='bestand' type='file' name='fileToUpload' id='fileToUpload'><br>
-                <input class='upload' type='submit' value='Bewerk' name='submit'>
-                <a type='button' class='sluitknop' href='#'>&times;</a>
-            </form>
-        </div>
-    </div>
-
-
-</body>
-</html>";
-  }}
+//
+if(isset($_GET['id'])){
+    $id= $_GET['id'];
+    $sql= "SELECT * FROM products where id=$id";
+    $res = $conn->query($sql);
+    if ($res) {
+        foreach ($res as $result) {
+            echo"
+                <div class='container form-popup' id='bewerk'>
+                    <div class='form'>
+                        <form action='KOEL.php?id=$id' method='post' enctype='multipart/form-data'>
+                            <input type='text' name='name' class='input' placeholder='".$result['name']."'><br>
+                        
+                            <input class='input' type='name' name='price' placeholder='Prijs'><br>
+                            <input class='bestand' type='file' name='fileToUpload' id='fileToUpload'><br>
+                            <input class='upload' type='submit' value='Bewerk' name='submit'>
+                            <a type='button' class='sluitknop' href='/CCS/#'>&times;</a>
+                        </form>
+                    </div>
+                </div>
+            </body>
+            </html>";
+        }
+    }
+}
