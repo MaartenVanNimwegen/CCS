b2235a98804595137a2fa3870f28decfd4bf81a5
Christianfk2905 <c.f.koopman2905@gmail.com>
2022-03-01 14:29:46 +0100

    kool

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername="localhost";
-$username="deb85590_p31t2";
-$password="MiQBz8k3";
-$db="deb85590_p31t2";
+$username="root";
+$password="";
+$db="onlineshop";
 
 $conn= new mysqli($servername, $username, $password, $db);
 if(!$conn){
