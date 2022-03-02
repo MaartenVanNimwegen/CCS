e5d5b349e59ae99f7b3715cfb35100e50d16f209
Remon Dollenkamp <remon2004@outlook.com>
2022-03-02 09:00:10 +0100

    s

--- loguit.php
+++ loguit.php
@@ -3,6 +3,6 @@
    unset($_SESSION["admin_name"]);
    unset($_SESSION["user_name"]);
    
-   echo "<script>alert('Je bent uitgelogd')</script>";
+   echo "<script>confirm('Log uit?')</script>";
    header('Refresh: 0.1; URL = index.php');
 ?>
