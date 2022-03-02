4fec632630cb82fabb90886e0d7c9482d7accb89
Yasser30007 <234880@rocfriesepoort.nl>
2022-03-02 12:50:33 +0100

    Update header.php

--- header.php
+++ header.php
@@ -34,7 +34,7 @@ include 'connection.php';
     if (isset($_SESSION['admin_name'])) {
     } else {
       if (!empty($_SESSION["shopping_cart"])) {
-        $cart_count = count(array_keys($_SESSION["shopping_cart"])); ?><div class="cart_div"><a href="cart.php"><i class="fa fa-shopping-cart carticonn"></i> Cart<span><?php echo $cart_count; ?></span></a></div><?php
+        $cart_count = count(array_keys($_SESSION["shopping_cart"])); ?><div class="cart_div"><a href="cart.php"><i class="fa fa-shopping-cart carticonn"></i><span><?php echo $cart_count; ?></span></a></div><?php
     }} ?>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
       <i class="fa fa-bars"></i>
