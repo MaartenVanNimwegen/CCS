d5af6a09d7a7f3b953f6f6a18ee51740ef41992f
Yasser30007 <234880@rocfriesepoort.nl>
2022-03-07 13:42:31 +0100

    Update login.php

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>login form</title>
+   <title>CCS</title>
 
    
    <link rel="stylesheet" href="style/login.css">
